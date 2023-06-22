d4e926a0963c634f4073482d5bfa693bee9ffc64
CaptainChol <emmanuel.marial@student.moringaschool.com>
2023-06-22 10:46:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 12;
+const num4 = 8;
+const mod = num3 % num4;
+
+const numbers = [1, 5, 10, 20];
+const max = Math.max(...numbers);
+
+console.log(multiply); // 62
+console.log(random); // A random integer greater than 0
+console.log(mod); // 4
+console.log(max); // 20
